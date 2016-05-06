c064693bd73f5dbe36bd358ba9ca3237a10c6aab
Vinder <ornitier62@gmail.com>
2016-05-06 03:34:19 +0300

    alfa fin clean

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,22 @@ $(function(){
     //setTimeout(function(){
     //    $('.my-preloader').fadeOut();
     //}, 7000)
+
+    $("form").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            alert("Thank you!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
+    $("img, a").on("dragstart", function(event) { event.preventDefault(); });
 });
